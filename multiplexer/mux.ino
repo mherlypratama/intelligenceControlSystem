f7e03a721340bd92c2869f62bbc8559f8eb32e74
mherlypratama <mherlypratama2033@students.unila.ac.id>
2023-11-12 15:27:49 +0700

    cekAll

--- multiplexer/mux.ino
+++ multiplexer/mux.ino
@@ -1,15 +1,29 @@
 #include "MUX74HC4067.h"
-MUX74HC4067 DigitalMux(11, 10, 9, 13, 12);
+MUX74HC4067 DigitalMux(13, 12, 14, 27, 26);
 int PIN;
 
 void setup()
 {
-    DigitalMux.signalPin(14, INPUT_PULLUP, DIGITAL);
+    DigitalMux.signalPin(25, INPUT_PULLUP, DIGITAL);
     Serial.begin(9600);
 }
 
 void loop()
 {
-    PIN = DigitalMux.read(2);
-    Serial.println(PIN);
+    Serial.println("==0123456789012345==");
+    Serial.print("  ");
+    for (byte i = 0; i < 16; i++)
+    {
+        data = DigitalMux.read(i);
+        if (data == HIGH)
+        {
+            Serial.print("1");
+        }
+
+        else
+        {
+            Serial.print("0");
+        }
+    }
+    Serial.println();
 }
