dfebad6e9943e1318d6f858203be215dbb60d039
mherlypratama <mherlypratama2033@students.unila.ac.id>
2023-11-24 00:19:23 +0700

    repair

--- allToran/coba.ino
+++ allToran/coba.ino
@@ -113,31 +113,15 @@ void printLocalTime()
         Serial.println("Failed to obtain time");
         return;
     }
-    Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
-    Serial.print("Day of week: ");
-    Serial.println(&timeinfo, "%A");
-    Serial.print("Month: ");
-    Serial.println(&timeinfo, "%B");
-    Serial.print("Day of Month: ");
-    Serial.println(&timeinfo, "%d");
-    Serial.print("Year: ");
-    Serial.println(&timeinfo, "%Y");
-    Serial.print("Hour: ");
-    Serial.println(&timeinfo, "%H");
-    Serial.print("Hour (12 hour format): ");
-    Serial.println(&timeinfo, "%I");
-    Serial.print("Minute: ");
-    Serial.println(&timeinfo, "%M");
-    Serial.print("Second: ");
-    Serial.println(&timeinfo, "%S");
 
-    Serial.println("Time variables");
-    char timeHour[3];
-    strftime(timeHour, 3, "%H", &timeinfo);
-    Serial.println(timeHour);
-    char timeWeekDay[10];
-    strftime(timeWeekDay, 10, "%A", &timeinfo);
-    Serial.println(timeWeekDay);
+    tanggal = &timeinfo, "%A, %B %d %Y %H:%M:%S";
+    Serial.println();
+
+    char strftime_buf[64];
+
+    // Format waktu sesuai keinginan Anda, contoh: "Sabtu, 06 November 2023 14:30:45"
+    strftime(strftime_buf, sizeof(strftime_buf), "%A, %d %B %Y %H:%M:%S", &timeinfo);
+    Serial.println(strftime_buf);
     Serial.println();
 }
 
--- waktu/waktubenar.ino
+++ waktu/waktubenar.ino
@@ -8,10 +8,30 @@ const char *ntpServer = "pool.ntp.org";
 const long gmtOffset_sec = 21600;
 const int daylightOffset_sec = 3600;
 
+int jam, minute, second, tanggal, bulan, tahun;
+
 void setup()
 {
     Serial.begin(9600);
 
+    // Init and get the time
+    setwifi();
+    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+    printLocalTime();
+
+    // // disconnect WiFi as it's no longer needed
+    // WiFi.disconnect(true);
+    // WiFi.mode(WIFI_OFF);
+}
+
+void loop()
+{
+    delay(1000);
+    printLocalTime();
+}
+
+void setwifi()
+{
     // Connect to Wi-Fi
     Serial.print("Connecting to ");
     Serial.println(ssid);
@@ -23,20 +43,6 @@ void setup()
     }
     Serial.println("");
     Serial.println("WiFi connected.");
-
-    // Init and get the time
-    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-    printLocalTime();
-
-    // disconnect WiFi as it's no longer needed
-    WiFi.disconnect(true);
-    WiFi.mode(WIFI_OFF);
-}
-
-void loop()
-{
-    delay(1000);
-    printLocalTime();
 }
 
 void printLocalTime()
@@ -48,13 +54,15 @@ void printLocalTime()
         return;
     }
 
-    tanggal = &timeinfo, "%A, %B %d %Y %H:%M:%S";
-    Serial.println();
+    jam = timeinfo.tm_hour;
+    minute = timeinfo.tm_min;
+    second = timeinfo.tm_sec;
 
-    char strftime_buf[64];
+    tanggal = timeinfo.tm_mday;
+    bulan = timeinfo.tm_mon + 1;     // Bulan dimulai dari 0, sehingga Anda perlu menambahkan 1
+    tahun = 1900 + timeinfo.tm_year; // Tahun dimulai dari 1900
 
-    // Format waktu sesuai keinginan Anda, contoh: "Sabtu, 06 November 2023 14:30:45"
+    char strftime_buf[50]; // Buffer untuk menyimpan timestamp yang diformat
     strftime(strftime_buf, sizeof(strftime_buf), "%A, %d %B %Y %H:%M:%S", &timeinfo);
     Serial.println(strftime_buf);
-    Serial.println();
 }
