fb56be68c6b8091af71b3ffdbcf0e6dd5d169cf9
mherlypratama <mherlypratama2033@students.unila.ac.id>
2023-12-19 18:49:19 +0700

    update

--- Relay/relayWaktu.ino
+++ Relay/relayWaktu.ino
@@ -7,15 +7,28 @@ const char *password = "telekomunikasi";
 const char *ntpServer = "pool.ntp.org";
 const long gmtOffset_sec = 21600;
 const int daylightOffset_sec = 3600;
-#define RELAY_PIN 16
 #define RELAY_PIN2 17
+#define RELAY_PIN3 16
 
 void setup()
 {
     Serial.begin(9600);
-    pinMode(RELAY_PIN, OUTPUT);
     pinMode(RELAY_PIN2, OUTPUT);
+    pinMode(RELAY_PIN3, OUTPUT);
+    setwifi();
+    // Init and get the time
+    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+    printLocalTime();
+}
 
+void loop()
+{
+    delay(1000);
+    printLocalTime();
+}
+
+void setwifi()
+{
     // Connect to Wi-Fi
     Serial.print("Connecting to ");
     Serial.println(ssid);
@@ -27,20 +40,6 @@ void setup()
     }
     Serial.println("");
     Serial.println("WiFi connected.");
-
-    // Init and get the time
-    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-    printLocalTime();
-
-    // disconnect WiFi as it's no longer needed
-    WiFi.disconnect(true);
-    WiFi.mode(WIFI_OFF);
-}
-
-void loop()
-{
-    delay(1000);
-    printLocalTime();
 }
 
 void printLocalTime()
@@ -52,36 +51,69 @@ void printLocalTime()
         return;
     }
 
+    jam = timeinfo.tm_hour;
+    minute = timeinfo.tm_min;
+    second = timeinfo.tm_sec;
+
+    tanggal = timeinfo.tm_mday;
+    bulan = timeinfo.tm_mon + 1;     // Bulan dimulai dari 0, sehingga Anda perlu menambahkan 1
+    tahun = 1900 + timeinfo.tm_year; // Tahun dimulai dari 1900
+}
+
+// Nutrisi
+void relay22()
+{
+    struct tm timeinfo;
+    if (!getLocalTime(&timeinfo))
+    {
+        Serial.println("Failed to obtain time");
+        return;
+    }
     // Extract the hour (0-23) from the struct tm
     int jam = timeinfo.tm_hour;
     int minute = timeinfo.tm_min;
 
-    // Print the extracted hour
-    Serial.print("Hour: ");
-    Serial.println(jam);
-
-    // Add your conditions based on the 'jam' variable here
-    if (jam >= 22 && minute >= 27 && minute < 28)
-    {
-        // Morning
-        digitalWrite(RELAY_PIN, LOW);
-    }
-
-    else if (jam >= 22 && minute >= 28 && minute < 29)
+    // Kontrol RELAY_PIN22
+    // hidup saat jam 16,6,10,1 siang. dan hidup selama 15 menit
+    if (jam == 16 && minute >= 1 && minute < 16 || jam == 6 && minute >= 1 && minute < 16 || jam == 10 && minute >= 1 && minute < 16 || jam == 13 && minute >= 1 && minute < 16)
     {
         digitalWrite(RELAY_PIN2, LOW);
-        // Afternoon
+        relay2 = 1;
+        Serial.println(relay2);
     }
-
     else
     {
-        // Evening
-        digitalWrite(RELAY_PIN, HIGH);
+        digitalWrite(RELAY_PIN2, HIGH);
+        relay2 = 0;
+        Serial.println(relay2);
     }
+}
 
-    // Print other time information
-    Serial.print("Minute: ");
-    Serial.println(timeinfo.tm_min);
-    Serial.print("Second: ");
-    Serial.println(timeinfo.tm_sec);
+// air pendingin
+// ada variabel 'temperature' ganti variable ini untuk acuan menghidupkan pompa pendingin
+void relay33()
+{
+    struct tm timeinfo;
+    if (!getLocalTime(&timeinfo))
+    {
+        Serial.println("Failed to obtain time");
+        return;
+    }
+    // Extract the hour (0-23) from the struct tm
+    int jam = timeinfo.tm_hour;
+    int minute = timeinfo.tm_min;
+
+    // Kontrol RELAY_PIN2
+    if (temperature > 32)
+    {
+        digitalWrite(RELAY_PIN3, LOW);
+        relay3 = 1;
+        Serial.println(relay3);
+    }
+    else
+    {
+        digitalWrite(RELAY_PIN3, HIGH);
+        relay3 = 0;
+        Serial.println(relay3);
+    }
 }
