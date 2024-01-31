e40508486bea7976c3595a22eeed6688bb4a29cb
mherlypratama <mherlypratama2033@students.unila.ac.id>
2024-01-31 16:35:34 +0700

    wind

--- allPertanian/Benar/newPCB/esp1.ino
+++ allPertanian/Benar/newPCB/esp1.ino
@@ -2,6 +2,22 @@
 #include "EEPROM.h"
 #include <OneWire.h>
 #include <SoftwareSerial.h>
+#include "RS485_Wind_Direction_Transmitter_V2.h"
+
+#if defined(ARDUINO_AVR_UNO) || defined(ESP8266) // Use softserial
+SoftwareSerial softSerial(/*rx =*/13, /*tx =*/19);
+RS485_Wind_Direction_Transmitter_V2 windDirection(/*softSerial =*/&softSerial);
+#elif defined(ESP32) // Use the hardserial of remappable pin: Serial1
+RS485_Wind_Direction_Transmitter_V2 windDirection(/*hardSerial =*/&Serial1, /*rx =*/13, /*tx =*/19);
+#else                // Use hardserial: Serial1
+RS485_Wind_Direction_Transmitter_V2 windDirection(/*hardSerial =*/&Serial1);
+#endif
+
+uint8_t Address = 0x02;
+
+const char *Orientation[17] = {
+    "north", "north by northeast", "northeast", "east by northeast", "east", "east by southeast", "southeast", "south by southeast", "south",
+    "south by southwest", "southwest", "west by southwest", "west", "west by northwest", "northwest", "north by northwest", "north"};
 
 // *******************PIN************************
 #define LED_BUILTIN 2
@@ -81,7 +97,7 @@ void IRAM_ATTR pulseCounter4()
 }
 
 // Variabel Utama
-float temperatureair, phValue, tdsValue = 0;
+float temperatureair, phValue, tdsValue = 0, Angle;
 float flowRate1, flowRate2, flowRate3, flowRate4;
 
 void setup()
@@ -92,6 +108,7 @@ void setup()
     settds();
     setwater();
     setanemo();
+    setwind();
 }
 
 void loop()
@@ -101,6 +118,31 @@ void loop()
     sensortds();
     sensorwater();
     sensoranemo();
+    sensorwind();
+}
+
+void setwind()
+{
+    // Init the sensor
+    while (!(windDirection.begin()))
+    {
+        Serial.println("Communication with device failed, please check connection");
+        delay(3000);
+    }
+    Serial.println("Begin ok!");
+}
+
+void sensorwind()
+{
+    // Get 16 wind directions
+    int Direction = windDirection.GetWindDirection(/*modbus slave address*/ Address);
+    // Get 360° wind direction angle
+    Angle = windDirection.GetWindAngle(/*modbus slave address*/ Address);
+    Serial.println(Orientation[Direction]);
+    Serial.print(Angle);
+    Serial.println("°");
+    Serial.println();
+    delay(1000);
 }
 
 void setanemo()
