6e4401148acef440ae303a0529e324bab79caf34
mherlypratama <mherlypratama2033@students.unila.ac.id>
2024-02-28 15:04:22 +0700

    lagi

--- allPertanian/Benar/newPCB/coba1.ino
+++ allPertanian/Benar/newPCB/coba1.ino
@@ -9,6 +9,18 @@ RS485_Wind_Direction_Transmitter_V2 windDirection(/*hardSerial =*/&Serial1, /*rx
 RS485_Wind_Direction_Transmitter_V2 windDirection(/*hardSerial =*/&Serial1);
 #endif
 
+//*****************PIN*****************************
+SoftwareSerial mySerial2(16, 17); // Define the soft serial port, port 3 is TX, port 2 is RX,
+#define TdsSensorPin A1
+
+#define VREF 5.0          // analog reference voltage(Volt) of the ADC
+#define SCOUNT 30         // sum of sample point
+int analogBuffer[SCOUNT]; // store the analog value in the array, read from ADC
+int analogBufferTemp[SCOUNT];
+int analogBufferIndex = 0, copyIndex = 0;
+float averageVoltage = 0, tdsValue = 0, temperaturetds = 25;
+uint8_t Address0 = 0x10;
+
 uint8_t Address = 0x02;
 
 const char *Orientation[17] = {
@@ -19,11 +31,62 @@ void setup()
 {
     Serial.begin(9600);
     setwind();
+    setanemo();
+    settds();
 }
 
 void loop()
 {
+    sensortds();
     sensorwind();
+    sensoranemo();
+}
+
+void settds()
+{
+    pinMode(TdsSensorPin, INPUT);
+}
+
+void sensortds()
+{
+    static unsigned long analogSampleTimepoint = millis();
+    if (millis() - analogSampleTimepoint > 40U) // every 40 milliseconds,read the analog value from the ADC
+    {
+        analogSampleTimepoint = millis();
+        analogBuffer[analogBufferIndex] = analogRead(TdsSensorPin); // read the analog value and store into the buffer
+        analogBufferIndex++;
+        if (analogBufferIndex == SCOUNT)
+            analogBufferIndex = 0;
+    }
+    static unsigned long printTimepoint = millis();
+    if (millis() - printTimepoint > 800U)
+    {
+        printTimepoint = millis();
+        for (copyIndex = 0; copyIndex < SCOUNT; copyIndex++)
+            analogBufferTemp[copyIndex] = analogBuffer[copyIndex];
+        averageVoltage = getMedianNum(analogBufferTemp, SCOUNT) * (float)VREF / 1024.0;                                                                                                  // read the analog value more stable by the median filtering algorithm, and convert to voltage value
+        float compensationCoefficient = 1.0 + 0.02 * (temperaturetds - 25.0);                                                                                                            // temperaturetds compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.02*(fTP-25.0));
+        float compensationVolatge = averageVoltage / compensationCoefficient;                                                                                                            // temperaturetds compensation
+        tdsValue = (133.42 * compensationVolatge * compensationVolatge * compensationVolatge - 255.86 * compensationVolatge * compensationVolatge + 857.39 * compensationVolatge) * 0.5; // convert voltage value to tds value
+        // Serial.print("voltage:");
+        // Serial.print(averageVoltage,2);
+        // Serial.print("V   ");
+        Serial.print("TDS Value:");
+        Serial.print(tdsValue, 0);
+        Serial.println("ppm");
+    }
+}
+
+void setanemo()
+{
+    mySerial2.begin(9600);
+    ModifyAddress(0x00, Address0);
+}
+
+void sensoranemo()
+{
+    Serial.print(readWindSpeed(Address0)); // Read wind speed
+    Serial.println("m/s");
 }
 
 void setwind()
@@ -49,4 +112,252 @@ void sensorwind()
     Serial.println("°");
     Serial.println();
     delay(1000);
+}
+
+int getMedianNum(int bArray[], int iFilterLen)
+{
+    int bTab[iFilterLen];
+    for (byte i = 0; i < iFilterLen; i++)
+        bTab[i] = bArray[i];
+    int i, j, bTemp;
+    for (j = 0; j < iFilterLen - 1; j++)
+    {
+        for (i = 0; i < iFilterLen - j - 1; i++)
+        {
+            if (bTab[i] > bTab[i + 1])
+            {
+                bTemp = bTab[i];
+                bTab[i] = bTab[i + 1];
+                bTab[i + 1] = bTemp;
+            }
+        }
+    }
+    if ((iFilterLen & 1) > 0)
+        bTemp = bTab[(iFilterLen - 1) / 2];
+    else
+        bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
+    return bTemp;
+}
+
+size_t readN(uint8_t *buf, size_t len)
+{
+    size_t offset = 0, left = len;
+    int16_t Tineout = 1500;
+    uint8_t *buffer = buf;
+    long curr = millis();
+    while (left)
+    {
+        if (mySerial2.available())
+        {
+            buffer[offset] = mySerial2.read();
+            offset++;
+            left--;
+        }
+        if (millis() - curr > Tineout)
+        {
+            break;
+        }
+    }
+    return offset;
+}
+
+/**
+   @brief 	Calculate CRC16_2 check value
+   @param buf 		Packet for calculating the check value
+   @param len 		Length of data that needs to check
+   @return 		Return a 16-bit check result.
+*/
+uint16_t CRC16_2(uint8_t *buf, int16_t len)
+{
+    uint16_t crc = 0xFFFF;
+    for (int pos = 0; pos < len; pos++)
+    {
+        crc ^= (uint16_t)buf[pos];
+        for (int i = 8; i != 0; i--)
+        {
+            if ((crc & 0x0001) != 0)
+            {
+                crc >>= 1;
+                crc ^= 0xA001;
+            }
+            else
+            {
+                crc >>= 1;
+            }
+        }
+    }
+
+    crc = ((crc & 0x00ff) << 8) | ((crc & 0xff00) >> 8);
+    return crc;
+}
+
+/**
+   @brief  Adds a CRC_16 check to the end of the packet
+   @param buf 		Data packet that needs to add the check value
+   @param len 		Length of data that needs to add check
+   @return 		None
+*/
+void addedCRC(uint8_t *buf, int len)
+{
+    uint16_t crc = 0xFFFF;
+    for (int pos = 0; pos < len; pos++)
+    {
+        crc ^= (uint16_t)buf[pos];
+        for (int i = 8; i != 0; i--)
+        {
+            if ((crc & 0x0001) != 0)
+            {
+                crc >>= 1;
+                crc ^= 0xA001;
+            }
+            else
+            {
+                crc >>= 1;
+            }
+        }
+    }
+    buf[len] = crc % 0x100;
+    buf[len + 1] = crc / 0x100;
+}
+
+/**
+   @brief 	Read the wind speed
+   @param Address0 	The read device address0
+   @return 		Wind speed unit m/s, return -1 for read timeout
+*/
+
+float readWindSpeed(uint8_t Address0)
+{
+    uint8_t Data[7] = {0};                                             // Store the original data packet returned by the sensor
+    uint8_t COM[8] = {0x00, 0x03, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00}; // Command for reading wind speed
+    boolean ret = false;                                               // Wind speed acquisition success flag
+    float WindSpeed = 0;
+    long curr = millis();
+    long curr1 = curr;
+    uint8_t ch = 0;
+    COM[0] = Address0;       // Add the complete command package with reference to the communication protocol.
+    addedCRC(COM, 6);        // Add CRC_16 check for reading wind speed command packet
+    mySerial2.write(COM, 8); // Send the command of reading the wind speed
+
+    while (!ret)
+    {
+        if (millis() - curr > 1000)
+        {
+            WindSpeed = -1; // If the wind speed has not been read for more than 1000 milliseconds, it will be regarded as a timeout and return -1.
+            break;
+        }
+
+        if (millis() - curr1 > 100)
+        {
+            mySerial2.write(COM, 8); // If the last command to read the wind speed is sent for more than 100 milliseconds and the return command has not been received, the command to read the wind speed will be re-sent
+            curr1 = millis();
+        }
+
+        if (readN(&ch, 1) == 1)
+        {
+            if (ch == Address0)
+            { // Read and judge the packet header.
+                Data[0] = ch;
+                if (readN(&ch, 1) == 1)
+                {
+                    if (ch == 0x03)
+                    { // Read and judge the packet header.
+                        Data[1] = ch;
+                        if (readN(&ch, 1) == 1)
+                        {
+                            if (ch == 0x02)
+                            { // Read and judge the packet header.
+                                Data[2] = ch;
+                                if (readN(&Data[3], 4) == 4)
+                                {
+                                    if (CRC16_2(Data, 5) == (Data[5] * 256 + Data[6]))
+                                    { // Check data packet
+                                        ret = true;
+                                        WindSpeed = (Data[3] * 256 + Data[4]) / 10.00; // Calculate the wind speed
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return WindSpeed;
+}
+
+/**
+   @brief 	Modify the sensor device address
+   @param Address1 	The address of the device before modification. Use the 0x00 address to set any address, after setting, you need to re-power on and restart the module.
+   @param Address2 	The modified address of the device, the range is 0x00~0xFF,
+   @return 		Returns true to indicate that the modification was successful, and returns false to indicate that the modification failed.
+*/
+
+boolean ModifyAddress(uint8_t Address1, uint8_t Address2)
+{
+    uint8_t ModifyAddressCOM[11] = {0x00, 0x10, 0x10, 0x00, 0x00, 0x01, 0x02, 0x00, 0x00, 0x00, 0x00};
+    boolean ret = false;
+    long curr = millis();
+    long curr1 = curr;
+    uint8_t ch = 0;
+    ModifyAddressCOM[0] = Address1;
+    ModifyAddressCOM[8] = Address2;
+    addedCRC(ModifyAddressCOM, 9);
+    mySerial2.write(ModifyAddressCOM, 11);
+    while (!ret)
+    {
+        if (millis() - curr > 1000)
+        {
+            break;
+        }
+
+        if (millis() - curr1 > 100)
+        {
+            mySerial2.write(ModifyAddressCOM, 11);
+            curr1 = millis();
+        }
+
+        if (readN(&ch, 1) == 1)
+        {
+            if (ch == Address1)
+            {
+                if (readN(&ch, 1) == 1)
+                {
+                    if (ch == 0x10)
+                    {
+                        if (readN(&ch, 1) == 1)
+                        {
+                            if (ch == 0x10)
+                            {
+                                if (readN(&ch, 1) == 1)
+                                {
+                                    if (ch == 0x00)
+                                    {
+                                        if (readN(&ch, 1) == 1)
+                                        {
+                                            if (ch == 0x00)
+                                            {
+                                                if (readN(&ch, 1) == 1)
+                                                {
+                                                    if (ch == 0x01)
+                                                    {
+                                                        // while (1) {
+                                                        Serial.println("Please power on the sensor again.");
+                                                        // delay(1000);
+                                                        // }
+                                                        ret = true;
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return ret;
 }
