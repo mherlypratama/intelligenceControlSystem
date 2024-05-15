37152f3616679ab9ac33b6137a1105fb1733b942
mherlypratama <mherlypratama2033@students.unila.ac.id>
2024-05-15 16:42:08 +0700

    update

--- excel/template.py
+++ excel/template.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 # Workbook() takes one, non-optional, argument
 # which is the filename that we want to create.
-workbook = xlsxwriter.Workbook("template.xlsx")
+workbook = xlsxwriter.Workbook("des-11.xlsx")
 
 # The workbook object is then used to add new
 # worksheet via the add_worksheet() method.
@@ -17,9 +17,10 @@ tanggal = list(range(1, 32))
 for b in tanggal:
     for c in range(24):
         for d in range(60):
-            current_time = datetime(2023, 11, 29, c, d, random.choice(range(60)))
+            current_time = datetime(2023, 12, 11, c, d, random.choice(range(60)))
             timestamp = current_time.strftime("%Y-%m-%d %H:%M:%S")
             worksheet.write(f"A{(b - 1) * 1440 + c * 60 + d + 1}", timestamp)
+
 
 # Finally, close the Excel file
 # via the close() method.
